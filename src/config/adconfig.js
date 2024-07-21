1f4df0abc1f523bf5aa023251e651a938d40c295
=empiretylh <=thuralinhtut3@gmail.com>
2024-07-21 23:12:17 +0630

    ADCONFIG

--- src/config/adconfig.js
+++ src/config/adconfig.js
@@ -1,9 +1,9 @@
 import { TestIds } from "react-native-google-mobile-ads"
 
 export const ADUNIT  = {
-    appopenunit : __DEV__ ? TestIds.APP_OPEN : 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy',
-    bannerunit :__DEV__ ? TestIds.ADAPTIVE_BANNER :  'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy',
-    interstitial : __DEV__ ? TestIds.INTERSTITIAL : TestIds?.INTERSTITIAL
+    appopenunit : __DEV__ ? TestIds.APP_OPEN : 'ca-app-pub-9596753066914925/1011224259',
+    bannerunit :__DEV__ ? TestIds.ADAPTIVE_BANNER :  'ca-app-pub-9596753066914925/6103768832',
+    interstitial : __DEV__ ? TestIds.INTERSTITIAL : 'ca-app-pub-9596753066914925/7873012045'
 }
 
 export const ADKEYWORD = ['fashion', 'electronic', 'mobile']
